8e635e5cef4c6a5831cf05132eedf186622e42f9
ChedlyMejri <83920557+ChedlyMejri@users.noreply.github.com>
2021-05-09 19:44:59 +0100

    Add files via upload

---
+++ Front/src/app/recomapprenant/recomapprenant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recomapprenant',
+  templateUrl: './recomapprenant.component.html',
+  styleUrls: ['./recomapprenant.component.css']
+})
+export class RecomapprenantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
